caa9ddc4ea5e46e9301945060e24cebab2a4668c
nithyajaya <31317243+nithyajaya@users.noreply.github.com>
2021-06-13 19:36:03 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,5 @@
-package com.mycompany.app;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+class HelloWorld{
+	public static void main(String a[]){
+		System.out.println("Hello World");
+	}
 }
